bbd42a760bcd0dd7f70927d0ed3d82f51d249c7c
Cristi <cgritco1998@gmail.com>
2026-04-14 17:58:42 +0300

    First Java Project

---
+++ CoursePractice/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java World!\n");
+
+        NewClass newClass = new NewClass("Java");
+        newClass.printName();
+
+        NewClass newClass1 = new NewClass("TekWill");
+        newClass1.printName();
+    }
+}
---
+++ CoursePractice/src/NewClass.java
@@ -0,0 +1,12 @@
+public class NewClass {
+
+    String name;
+
+    public NewClass(String name) {
+        this.name = name;
+    }
+
+    public void printName() {
+        System.out.println(this.name + "\n");
+    }
+}
