e49adabbadc3fb4b18b389c0b5d3ab467cc902ad
Blabla792 <cgritco1998@gmail.com>
2026-06-04 21:01:41 +0300

    Add the displayPhoneAFriend method.

--- WhoWantsToBeAMillionaire/src/Main.java
+++ WhoWantsToBeAMillionaire/src/Main.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
-    Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         DataManage data = new DataManage();
         FiftyFiftyLifeline fiftyFifty =  new FiftyFiftyLifeline();
         PrizeManager prizeManager = new PrizeManager();
+        PhoneAFriend phoneAFriend = new PhoneAFriend();
 
         displayQuestion(data.getGameQuestions()[14], prizeManager);
 
         String[] f = fiftyFifty.getFiftyFiftyAnswers(data.getGameQuestions()[14].getAnswers());
-        displayFiftyFiftyAnswers(f);
+        displayFiftyFiftyLifeline(f);
+
+        displayPhoneAFriend(data.getGameQuestions()[2], phoneAFriend, prizeManager);
     }
 
     public static void displayQuestion(Question question, PrizeManager prizeManager) {
@@ -33,7 +33,7 @@ public class Main {
         System.out.println("\n");
     }
 
-    public static void displayFiftyFiftyAnswers(String[] answers) {
+    public static void displayFiftyFiftyLifeline(String[] answers) {
         System.out.println("--- 50/50 Lifeline Used ---");
 
         for (int i = 0; i < answers.length; i++) {
@@ -44,5 +44,43 @@ public class Main {
             System.out.printf("%c: %s", letter, answers[i]);
         }
         System.out.println("\n");
+    }
+
+    public static void displayPhoneAFriend(Question question, PhoneAFriend phoneAFriend, PrizeManager prizeManager) {
+        Answer answer = question.getAnswers();
+        String questionText = question.getQuestion();
+        String[] options = answer.options();
+
+        char suggestedLetter = phoneAFriend.getPhoneAFriendAnswer(answer);
+
+        int index = suggestedLetter - 'A';
+        String answerText = answer.options()[index];
+
+        int questionValue = prizeManager.getQuestionValue(question.getQuestionNumber());
+
+        System.out.println("--- Phone a Friend Used ---");
+        System.out.println("Ringing your friend...");
+        System.out.println("Friend: Hello?");
+        System.out.println("You: I need your help with a question for $" + questionValue + "!");
+        System.out.println("You: The question is: " + questionText);
+
+        System.out.println("You: The options are: ");
+        for (int i = 0; i < options.length; i++) {
+            char letter = (char) ('A' + i);
+            System.out.print(letter + ": " + options[i]);
+
+            if (i < options.length - 2) {
+                System.out.print(", ");
+            } else if (i == options.length - 2) {
+                System.out.print(", or ");
+            }
+        }
+        System.out.println(".");
+
+        if (phoneAFriend.isConfident()) {
+            System.out.println("Friend: Oh, I'm pretty sure I know this one. The answer is " + suggestedLetter + ": " + answerText);
+        } else {
+            System.out.println("Friend: Wow, that's tough. If I had to guess, I'd say " + suggestedLetter + ": " + answerText);
+        }
     }
 }
